9f8ecd6841e492941b00ad2853973df193ae8b44
Tian Luan <tiluan1999@hotmail.com>
2023-10-14 11:20:29 -0700

    feat: Init the project and create index page

---
+++ server.js
@@ -0,0 +1,45 @@
+const cors = require('cors');
+const next = require('next');
+const Pusher = require('pusher');
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const Sentiment = require('sentiment');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+
+const app = next({ dev });
+const handler = app.getRequestHandler();
+const sentiment = new Sentiment();
+
+// Configure the pusher
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: process.env.PUSHER_APP_CLUSTER,
+    useTLS: true
+});
+
+app.prepare().then(() => {
+    const server = express();
+
+    server.use(cors());
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: true }));
+
+    server.get('*', (req, res) => {
+        return handler(req, res);
+    });
+
+    // Start the server and listen to specific port
+    server.listen(port, err => {
+        if (err) throw err;
+        console.log(`> Server is ready to work`)
+    })
+})
+.catch(exception => { // Handle exceptions
+    console.error(exception.stack);
+    process.exit(1);
+})
