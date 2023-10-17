c3d1e1c58911c4cd06ca13a2aba3841a0fdba499
Tian Luan <tiluan1999@hotmail.com>
2023-10-17 01:08:27 -0700

    feat: Add chat history feature

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -1,6 +1,7 @@
 const Sentiment = require('sentiment');
 const Pusher = require('pusher');
 import { NextResponse } from 'next/server';
+import { sql } from '@vercel/postgres';
 
 const sentiment = new Sentiment();
 
@@ -15,14 +16,24 @@ const pusher = new Pusher({
 
 export async function POST(req) {
     console.log("Receive request POST /message")
-    const { user = null, message = '', timestamp = +new Date } = await req.json();
-    const sentimentScore = sentiment.analyze(message).score;
+    const { user = '', message = '', timestamp = +new Date } = await req.json();
+    const sentimentScore = sentiment.analyze(message).score || 0.0;
 
     const chat = { user, message, timestamp, sentiment: sentimentScore };
 
-    console.log(chat)
+    // Push message to database
+    try {
+        console.log("Try to send data to database")
+        await sql`INSERT INTO chat_history (chat_room, message, user_name, sentiment, timestamp) 
+            VALUES ('chat-room', ${chat.message}, ${chat.user}, ${chat.sentiment}, ${chat.timestamp});`;
+    } catch (exception) {
+        console.log("Failed to send data", exception)
+        return new NextResponse({
+            status: exception.status || 500,
+            body: exception.message
+        })
+    }
 
-    // chatHistory.messages.push(chat);
     await pusher.trigger('chat-room', 'new-message', { chat })
     .catch((exception) => {
         return new NextResponse({
--- src/app/api/messages/route.js
+++ src/app/api/messages/route.js
@@ -1,10 +1,35 @@
 import { NextResponse } from 'next/server';
+import { sql } from '@vercel/postgres';
 
 export async function POST(req) {
-    return new NextResponse({
+    console.log("Receive request POST /messages")
+
+    // Fetch chat history
+    let queryResult = []
+    try {
+        console.log("Try to fetch history data")
+        queryResult = await sql`SELECT user_name AS user, sentiment, timestamp, message
+                    FROM chat_history
+                    ORDER BY timestamp DESC
+                    LIMIT 20;`;
+    } catch (exception) {
+        console.log("Failed to fetch history data", exception)
+        return new NextResponse({
+            status: exception.status || 500,
+            body: exception.message
+        })
+    }
+
+    const chatHistory = queryResult.rows.map(row => ({
+        user: row.user,
+        sentiment: row.sentiment,
+        timestamp: row.timestamp,
+        message: row.message
+    }));
+
+    return NextResponse.json({
+        messages: chatHistory.reverse()
+      }, {
         status: 200,
-        body: {
-            messages: []
-        }
-    })
+      })
 }
